621163849b59a0400c522983a817d8f551a9e4b9
Rajanan <g.rajan1993@gmail.com>
2022-11-06 20:21:48 +0000

    fix vite docker + windows

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,26 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import type { UserConfig } from 'vite';
+import { resolve } from 'path';
 import postcss from './postcss.config.cjs';
 
 const config: UserConfig = {
 	plugins: [sveltekit()],
+	resolve: {
+		alias: {
+			$src: resolve('./src')
+		}
+	},
 	css: {
 		postcss
-	}
+	},
+	server: {
+		host: true,
+		strictPort: true,
+		port: 3000,
+		watch: {
+			usePolling: true,
+		},
+		},
 };
 
 export default config;
